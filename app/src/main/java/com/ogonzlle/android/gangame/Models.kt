74549462dd57022e48137fe212f114dca2afb8c0
Oscar Gonzalez Lesaga <Gonzox@MacBook-Pro-de-Oscar.local>
2018-07-01 19:42:11 +0200

    add structure files

---
+++ app/src/main/java/com/ogonzlle/android/gangame/Models.kt
@@ -0,0 +1,17 @@
+package com.ogonzlle.android.gangame
+
+data class Deal(var title: String,
+                var salePrice: Float,
+                var normalPrice: Float,
+                var metacriticScore: Int,
+                var steamRating: Int,
+                var thumb: String)
+
+data class TopGame(var title: String,
+                   var owners: Int,
+                   var steamRating: Int,
+                   var publisher: String,
+                   var price: Float,
+                   var position: Int,
+                   var thumb: String)
+
